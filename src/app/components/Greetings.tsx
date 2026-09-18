c88a848da1a45e7f79a910350221cca95eaa7dec
jaycode202 <jacksonbandax@gmail.com>
2026-09-18 13:33:00 +0200

    Implement dynamic greeting based on time of day

--- src/app/components/Greetings.tsx
+++ src/app/components/Greetings.tsx
@@ -1,1 +1,21 @@
+"use client";
 
+import { useEffect, useState } from "react";
+
+export default function Greeting() {
+  const [greeting, setGreeting] = useState("Good evening");
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+
+    if (hour >= 5 && hour < 12) {
+      setGreeting("Good morning");
+    } else if (hour >= 12 && hour < 17) {
+      setGreeting("Good afternoon");
+    } else {
+      setGreeting("Good evening");
+    }
+  }, []);
+
+  return <>{greeting}</>;
+}
